3dcc2a29bb1d6c8c4cbb2e7bbe39b03321bfc5f4
Michael L Perry <michael@qedcode.com>
2018-07-06 12:59:40 -0500

    Detect cycles.

--- MathematicalConcepts/UnitTest1.cs
+++ MathematicalConcepts/UnitTest1.cs
@@ -8,10 +8,21 @@ namespace MathematicalConcepts
     [TestClass]
     public class LazyTests
     {
+        //
+        // Compute the topological order for this graph:
+        //
+        //    A
+        //   ^ ^
+        //  /   \
+        // B     |
+        //  ^   /
+        //   \ /
+        //    C
+        //
         [TestMethod]
         public void LazyComputesTopologicalOrder()
         {
-            StringBuilder output = new StringBuilder();
+            var output = new StringBuilder();
             Lazy<int> a = null, b = null, c = null;
             c = new Lazy<int>(() => Evaluate(output, "C", () => a.Value + b.Value));
             b = new Lazy<int>(() => Evaluate(output, "B", () => a.Value + 3));
@@ -19,6 +30,39 @@ namespace MathematicalConcepts
 
             c.Value.Should().Be(7);
             output.ToString().Should().Be("<C><A></A><B></B></C>");
+        }
+
+        [TestMethod]
+        public void LazyComputesTopologicalOrderDifferently()
+        {
+            var output = new StringBuilder();
+            Lazy<int> a = null, b = null, c = null;
+            c = new Lazy<int>(() => Evaluate(output, "C", () => b.Value + a.Value));
+            b = new Lazy<int>(() => Evaluate(output, "B", () => a.Value + 3));
+            a = new Lazy<int>(() => Evaluate(output, "A", () => 2));
+
+            c.Value.Should().Be(7);
+            output.ToString().Should().Be("<C><B><A></A></B></C>");
+        }
+
+        //
+        // Find that this graph contains a cycle:
+        //
+        //    A
+        //   ^ |
+        //   | v
+        //    B
+        //
+        [TestMethod]
+        public void LazyDetectsCycles()
+        {
+            var output = new StringBuilder();
+            Lazy<int> a = null, b = null;
+            b = new Lazy<int>(() => Evaluate(output, "B", () => a.Value + 1));
+            a = new Lazy<int>(() => Evaluate(output, "A", () => b.Value - 1));
+
+            b.Invoking(l => { int v = l.Value; }).Should().Throw<InvalidOperationException>();
+            output.ToString().Should().Be("<B><A>");
         }
 
         private static int Evaluate(StringBuilder output, string name, Func<int> function)
